c68e07361c7c176f108cd6d6b57cb3a6151fbb0b
Pavel Semikin <semikinnumberone@gmail.com>
2020-10-25 01:32:41 +0300

    New images+create new block slider in html and css+ begin js code

---
+++ main.js
@@ -0,0 +1,7 @@
+const prev = document.querySelector('.control-prev');
+const next = document.querySelector('.control-next');
+const img = document.querySelectorAll('.slider-img');
+
+const moveBack = e => {};
+
+prev.addEventListener('click', moveBack);
