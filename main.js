30fec89cdaf7c1ab6e3f749b414c48329728471b
Pavel Semikin <semikinnumberone@gmail.com>
2020-10-25 18:50:10 +0200

    New js for move slider

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
 const prev = document.querySelector('.control-prev');
 const next = document.querySelector('.control-next');
-const img = document.querySelectorAll('.slider-img');
+const images = document.querySelectorAll('.slider-img');
 
-const moveBack = e => {};
+let startSlide = 0;
+
+const active = activeSlide => {
+  images.forEach(image => {
+    image.classList.remove('active');
+  });
+  images[activeSlide].classList.add('active');
+};
+
+const moveNext = () => {
+  if (startSlide === images.length - 1) {
+    startSlide = 0;
+    active(startSlide);
+  } else {
+    startSlide += 1;
+    active(startSlide);
+  }
+};
+
+const moveBack = () => {
+  if (startSlide === 0) {
+    startSlide = images.length - 1;
+    active(startSlide);
+  } else {
+    startSlide -= 1;
+    active(startSlide);
+  }
+};
 
 prev.addEventListener('click', moveBack);
+next.addEventListener('click', moveNext);
